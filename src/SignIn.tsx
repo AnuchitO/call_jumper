c9a929cc9ce5e0ee1ecc9e234bcba2fe24822951
Suzuki Aki <suzuki11109@gmail.com>
2016-05-16 22:15:48 +0700

    create sign in page

--- src/SignIn.tsx
+++ src/SignIn.tsx
@@ -2,20 +2,36 @@ import * as React from 'react';
 
 export default class SignIn extends React.Component<any, any> {
   render() {
+    var formStyle = {
+      maxWidth: 320,
+      margin: 'auto'
+    };
+
     return <div>
-      <h1>Welcome to Call Jumper</h1>
-      <div className="section">
-        <div>
-            <div className="input-field">
-              <input id="email" type="email" />
-              <label for="email">Email</label>
-            </div>
-            <div className="input-field">
-              <input id="password" type="password" />
-              <label for="password">Password</label>
-            </div>
-            <button className="waves-effect waves-light btn">Sign in</button>
+      <div className="section center-align">
+        <h3>Welcome to Call Jumper</h3>
+      </div>
+
+      <div className="section center-align" style={formStyle}>
+        <div className="input-field">
+          <input id="email" type="email" />
+          <label for="email">Email</label>
         </div>
+        <div className="input-field">
+          <input id="password" type="password" />
+          <label for="password">Password</label>
+        </div>
+        <div className="input-field">
+          <button className="waves-effect waves-light btn">Sign in</button>
+        </div>
+      </div>
+
+      <div className="section center-align">
+        <a href="#">Forget password?</a>
+      </div>
+
+      <div className="section center-align">
+        <a className="waves-effect waves-light btn">Create New Account</a>
       </div>
     </div>;
   }
